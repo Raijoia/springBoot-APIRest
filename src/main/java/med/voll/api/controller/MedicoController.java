a18d40aa40486667dadd6b418da41fc7caf37600
Rai Joia <megac154@gmail.com>
2023-11-06 16:55:24 -0300

    listagem de forma ordenada ok

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,9 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,8 +29,9 @@ public class MedicoController {
   }
 
   @GetMapping
-  public List<DadosListagemMedico> listar() {
+//  @PageableDefault() para filtrar a listagem, nesse caso, tamanho = 10 e arrumar a lista de forma alfabética pelo nome
+  public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
 //  pegando todos os medicos e transformando eles em um DadosListagemMedico com o map
-    return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    return repository.findAll(paginacao).map(DadosListagemMedico::new);
   }
 }
