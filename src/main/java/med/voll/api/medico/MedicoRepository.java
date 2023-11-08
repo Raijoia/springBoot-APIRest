cae8de9c3d9f1a73a90930cc7285fa80ac3ee321
Rai Joia <megac154@gmail.com>
2023-11-08 13:30:05 -0300

    criando metodo findAllByAtivoTrue, pra retornar apenas medicos ativo no get

--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -1,7 +1,12 @@
 package med.voll.api.medico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 //entidade e o tipo do ID da entidade
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+  Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
