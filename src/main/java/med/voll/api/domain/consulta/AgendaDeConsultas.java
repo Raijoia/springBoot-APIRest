423c91a73394e532bb74c421aec6c7e34b11d108
Rai Joia <megac154@gmail.com>
2023-11-23 11:13:53 -0300

    ativando os validadores e adicioando interface validador

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -2,11 +2,14 @@ package med.voll.api.domain.consulta;
 
 import jakarta.validation.ValidationException;
 import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.validacoes.ValidadorAgendamentoDeConsulta;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaDeConsultas {
@@ -16,6 +19,9 @@ public class AgendaDeConsultas {
   private MedicoRepository medicoRepository;
   @Autowired
   private PacienteRepository pacienteRepository;
+//  implementando os validadores pela interface
+  @Autowired
+  private List<ValidadorAgendamentoDeConsulta> validadores;
 
   public void agendar(DadosAgendamentoConsulta dados){
     if (!pacienteRepository.existsById(dados.idPaciente())){
@@ -25,6 +31,9 @@ public class AgendaDeConsultas {
     if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
       throw new ValidacaoException("Id do medico informado não existe!!");
     }
+
+//    validando todos os validadores usando o forEach e passando os dados
+    validadores.forEach(v -> v.validar(dados));
 
     var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
     var medico = escolherMedico(dados);
